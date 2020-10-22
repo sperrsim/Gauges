5f9fa5a4dd9206dbcb9ca630065a7ca637c897a6
Simon Sperr <simon.sperr@htl-wels.at>
2020-10-22 10:35:16 +0200

    Initial Commit

---
+++ src/kamiprsp/GaugeBase.java
@@ -0,0 +1,62 @@
+package kamiprsp;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
+public class GaugeBase extends Group {
+
+    private int width = 100;
+    private int height = 100;
+    private int value = 0;
+
+    public GaugeBase()
+    {
+        //nothing to do here
+    }
+
+    public void redraw()
+    {
+        Rectangle border = new Rectangle();
+        border.setX(0);
+        border.setY(0);
+        border.setWidth(width);
+        border.setHeight(height);
+        border.setArcWidth(20);
+        border.setArcHeight(20);
+        border.setStrokeWidth(10);
+        border.setStroke(Color.RED);
+        border.setFill(Color.GRAY);
+
+        Text t = new Text();
+        t.setText(String.valueOf(this.value));
+        t.setFont(Font.font ("Verdana", 20));
+        t.setFill(Color.BLACK);
+
+        //measure the text
+        double w = t.getLayoutBounds().getWidth();
+        double h = t.getLayoutBounds().getHeight();
+
+        //calculate center
+        t.setX(this.width / 2.0 - w / 2.0);
+        t.setY(this.height / 2 - h / 2.0);
+
+        this.getChildren().addAll(border, t);
+    }
+
+    public void init(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        this.redraw();
+    }
+
+    public void setValue(int v)
+    {
+        this.value = v;
+        this.redraw();
+    }
+}
---
+++ src/kamiprsp/Main.java
@@ -0,0 +1,65 @@
+package kamiprsp;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+    GaugeBase gb = new GaugeBase();
+    TextField txtValue = new TextField();
+
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        Application.launch(args);
+    }
+
+    @Override public void start(Stage stage) {
+
+        EventHandler<MouseEvent> btn_handler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+
+                try {
+                    int v = Integer.parseInt(txtValue.getText());
+                    gb.setValue(v);
+                }
+                catch(Exception ex) {
+                    System.out.println("Input Exception!");
+                }
+
+            }
+        };
+
+        gb.init(500, 500);
+        gb.setValue(45);
+
+        Button btnValue = new Button();
+        btnValue.setText("New Value");
+        btnValue.addEventHandler(MouseEvent.MOUSE_CLICKED, btn_handler);
+
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(10, 50, 50, 50));
+        vBox.setSpacing(20);
+        vBox.getChildren().addAll(gb, txtValue, btnValue);
+
+        //Creating a Scene
+        Scene scene = new Scene(vBox);
+
+        //Setting title to the scene
+        stage.setTitle("Gauge Collection");
+        //Adding the scene to the stage
+        stage.setScene(scene);
+
+        //Displaying the contents of a scene
+        stage.show();
+    }
+
+}

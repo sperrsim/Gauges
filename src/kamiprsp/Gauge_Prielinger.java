bc24ef7f58ba08d208f6a00f91e22845d99bdcc8
Marcel Prielinger <marcel.prielinger@htl-wels.at>
2020-11-03 17:33:39 +0100

    "Added Gauge_Prielinger"

--- src/kamiprsp/Gauge_Prielinger.java
+++ src/kamiprsp/Gauge_Prielinger.java
@@ -1,5 +1,8 @@
 package kamiprsp;
 
+import eu.hansolo.medusa.Gauge;
+import eu.hansolo.medusa.skins.*;
+
 /**
  * @class: Gauge_Prielinger
  * @author: Marcel Prielinger
@@ -8,5 +11,20 @@ package kamiprsp;
 
 public class Gauge_Prielinger extends GaugeBase {
 
-    
+    @Override
+    public void redraw() {
+
+        this.getChildren().clear();
+
+        Gauge gauge = new Gauge();
+
+        gauge.setValue(value);
+        gauge.setMaxHeight(height);
+        gauge.setMaxWidth(width);
+        gauge.setSkin(new AmpSkin(gauge));
+        gauge.setTitle("Prielinger Gauge");
+        gauge.setUnit("km/h");
+
+        this.getChildren().add(gauge);
+    }
 }

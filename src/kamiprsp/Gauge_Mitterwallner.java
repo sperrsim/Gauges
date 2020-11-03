b13f47e3e5d120057cf00fac4e911e18fabef920
Tobias Mitterwallner <tobias.mitterwallner@htl-wels.at>
2020-11-03 19:27:48 +0100

    "initial"

--- src/kamiprsp/Gauge_Mitterwallner.java
+++ src/kamiprsp/Gauge_Mitterwallner.java
@@ -1,9 +1,31 @@
 package kamiprsp;
 
+import eu.hansolo.medusa.Gauge;
+import eu.hansolo.medusa.skins.TileKpiSkin;
+import javafx.scene.paint.Color;
+
 /**
  * @author: Tobias Mitterwallner
  * @date: 22.10.2020
  */
 
-public class Gauge_Mitterwallner {
+public class Gauge_Mitterwallner
+{
+    public static Gauge e(String name, int value, int maxValue ) {
+        Gauge gauge = new Gauge();
+        //gauge.setSectionIconsVisible(false);
+        gauge.setPrefSize(300, 300);
+        gauge.setSkin(new TileKpiSkin(gauge));
+        gauge.setMaxValue(100);
+        gauge.setThreshold(60);
+        gauge.setTitle("this is it");
+        gauge.setValue(25);
+        gauge.setValueColor(Color.WHITE);
+        gauge.setTitleColor(Color.WHITE);
+        gauge.setThresholdVisible(true);
+        gauge.setThresholdColor(Color.RED);
+        //gauge.setSectionIconsVisible(false);
+        //gauge.setSectionsVisible(false);
+        return gauge;
+    }
 }
